7caae2021661d0e19e1e9e1d941ad4c6b24ae3a1
wallison <wallisongoncalves1105ibc@gmail.com>
2020-10-21 00:42:21 -0400

    mocks expectations

--- spec/mocks/mocks_spec.rb
+++ spec/mocks/mocks_spec.rb
@@ -11,4 +11,29 @@ describe 'Stubs' do
     #exercise
     student.bar
   end
+
+  it 'args' do
+    student = Student.new
+
+    expect(student).to receive(:foo).with(123)
+
+    student.foo(123)
+  end
+
+  it 'repeticao' do
+    student = Student.new
+
+    expect(student).to receive(:foo).with(123).twice
+
+    student.foo(123)
+    student.foo(123)
+  end
+
+  it 'retorno' do
+    student = Student.new
+
+    expect(student).to receive(:foo).with(123).and_return(true)
+
+    student.foo(123)
+  end
 end
